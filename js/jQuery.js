4fbae09bd873af76b9c5481dd23fac70ef3a51b6
mahmoud1zidan111 <mmaam2210@gmail.com>
2025-03-08 10:09:20 +0200

    jQuery animate

--- js/jQuery.js
+++ js/jQuery.js
@@ -1,14 +1,13 @@
 // jQuery animate
 
 $(document).ready(function () {
+  $(window).scrollTop(0);
   let had = $(".intro-text"); // اختيار العنصر باستخدام jQuery
 
-  had
-    .css({ display: "none", opacity: 0 }) // إخفاء العنصر وجعل الشفافية 0
-    .slideDown(1000, function () {
-      // إنزال العنصر من الأعلى للأسفل
-      $(this).animate({ opacity: 1 }, 1000, function () {
-        $("html, body").animate({ scrollTop: 476 }, 1500); // تمرير الصفحة بعد انتهاء التأثير
-      });
+  had.slideDown(1000, function () {
+    // إنزال العنصر من الأعلى للأسفل
+    $(this).animate({ opacity: 1 }, 1000, function () {
+      $("html, body").animate({ scrollTop: 476 }, 1500); // تمرير الصفحة بعد انتهاء التأثير
     });
+  });
 });
