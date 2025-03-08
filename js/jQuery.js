00e7f74b60b85a4cf8265f28bb6fdf642c8183a1
mahmoud1zidan111 <mmaam2210@gmail.com>
2025-03-08 11:46:27 +0200

    jQuery animate

--- js/jQuery.js
+++ js/jQuery.js
@@ -2,12 +2,16 @@
 
 $(document).ready(function () {
   $(window).scrollTop(0);
-  let had = $(".intro-text"); // اختيار العنصر باستخدام jQuery
-
+  let had = $(".intro-text");
   had.slideDown(1000, function () {
-    // إنزال العنصر من الأعلى للأسفل
-    $(this).animate({ opacity: 1 }, 1000, function () {
-      $("html, body").animate({ scrollTop: 476 }, 1500); // تمرير الصفحة بعد انتهاء التأثير
+    $(this).animate({ opacity: 1, bottom: 0 + "%" }, 1000, function () {
+      $("html, body").animate({ scrollTop: 650 }, 600, function () {
+        let myName = $("#all");
+        let img = $(".section-one-img");
+        $(img).animate({ left: 0 + "px", opacity: 1 }, 800, function () {
+          $(myName).animate({ right: 0, opacity: 1 }, 750);
+        });
+      });
     });
   });
 });
