49f1fb4306e512b24f7d9d2d99eba02cd81066fc
mahmoud1zidan111 <mmaam2210@gmail.com>
2025-03-11 09:05:31 +0200

    links

--- js/jQuery.js
+++ js/jQuery.js
@@ -1,16 +1,14 @@
 // jQuery animate
 
-$(document).ready(function () {
-  $(window).scrollTop(0);
-  let had = $(".intro-text");
-  had.slideDown(1000, function () {
-    $(this).animate({ opacity: 1, bottom: 0 + "%" }, 1000, function () {
-      $("html, body").animate({ scrollTop: 650 }, 600, function () {
-        let myName = $("#all");
-        let img = $(".section-one-img");
-        $(img).animate({ left: 0 + "px", opacity: 1 }, 800, function () {
-          $(myName).animate({ right: 0, opacity: 1 }, 750);
-        });
+let had = $(".intro-text");
+let img = $(".section-one-img");
+let myName = $("#all");
+// عرض العنوان مع تأثير الانزلاق ثم التمرير
+had.slideDown(1000, function () {
+  $(this).animate({ opacity: 1, bottom: "0%" }, 1000, function () {
+    $("html, body").animate({ scrollTop: 650 }, 600, function () {
+      img.animate({ left: "0px", opacity: 1 }, 800, function () {
+        myName.animate({ right: "0px", opacity: 1 }, 750);
       });
     });
   });
