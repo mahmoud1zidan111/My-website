39b8bc898934c337e82670b36338387f368110d2
mahmoud1zidan111 <mmaam2210@gmail.com>
2025-04-23 04:19:46 +0200

    scroll tob no

--- js/jQuery.js
+++ js/jQuery.js
@@ -3,13 +3,12 @@
 let had = $(".intro-text");
 let img = $(".section-one-img");
 let myName = $("#all");
-// عرض العنوان مع تأثير الانزلاق ثم التمرير
+
+// عرض العنوان مع تأثير الانزلاق ثم إظهار الصورة والاسم بتأثيرات
 had.slideDown(1000, function () {
   $(this).animate({ opacity: 1, bottom: "0%" }, 1000, function () {
-    $("html, body").animate({ scrollTop: 650 }, 600, function () {
-      img.animate({ left: "0px", opacity: 1 }, 800, function () {
-        myName.animate({ right: "0px", opacity: 1 }, 750);
-      });
+    img.animate({ left: "0px", opacity: 1 }, 800, function () {
+      myName.animate({ right: "0px", opacity: 1 }, 750);
     });
   });
 });
